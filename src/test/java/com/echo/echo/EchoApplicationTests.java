d41e7a4cb5d579f3c9e0044d4ab1ee242b0817b5
Melina Kiefer <melinakiefer@hotmail.de>
2025-01-30 14:34:50 +0100

    Alle restlichen Tests hinzugefügt. -> 100% Coverage erreicht

--- src/test/java/com/echo/echo/EchoApplicationTests.java
+++ src/test/java/com/echo/echo/EchoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.echo.echo;
 
+import static org.mockito.Mockito.mockStatic;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EchoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void testMainMethodCallsSpringApplicationRun() {
+        try (MockedStatic<SpringApplication> mockedStatic = mockStatic(SpringApplication.class)) {
+            String[] args = {};
+            EchoApplication.main(args);
+            mockedStatic.verify(() -> SpringApplication.run(EchoApplication.class, args));
+        }
+    }
 
 }
